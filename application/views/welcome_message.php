e991bcb921dfbecd09d02db33ce6d338c90a2287
Rukshan1203 <rukshansilva3@gmail.com>
2020-05-03 12:24:16 +0530

    add Title

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<link rel="icon" href="" type="image/gif" sizes="16x16">
-	<title>System Name</title>
+	<title>Web Base Commuter Ticketing System</title>
 	<!-- Tell the browser to be responsive to screen width -->
 	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 	<!-- Bootstrap 3.3.7 -->
